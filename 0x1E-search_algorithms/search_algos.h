a675784cb5b9e1eb63b2a8862d2f0b7522654701
anasdev92 <elyamni.anas@gmail.com>
2024-01-17 16:12:07 -0800

    0x1E-search_algorithms

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,23 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for algo_search project
+ */
+
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
